8ab70061315ee06b4be8c5b3785bebdb79d88ab9
Tuhin1904 <iamtuhinn@gmail.com>
2025-11-24 18:55:34 +0530

    New updated

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ const app = require("./app");
 const config = require("./config/config");
 const connect = require("./db/mongoose");
 
-async function start() {
+(async () => {
   try {
     await connect();
     console.log("Database is connected");
+
     app.listen(config.port, () => {
       console.log(`Server running on port ${config.port}`);
     });
   } catch (err) {
-    console.error("Failed to start:", err);
+    console.error("Failed to start server:", err);
+    process.exit(1);
   }
-}
+})();
 
-start();
+module.exports = app;
