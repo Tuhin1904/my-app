1391b4a52fe0bc27c5c0606be825817b150efdb2
Tuhin1904 <iamtuhinn@gmail.com>
2025-11-24 18:36:45 +0530

    connection route added to endure connect

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const userRoutes = require("./routes/user.routes");
-
+const connectDB = require("./db/mongoose");
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.get("/ping", (req, res) => {
+  if (connectDB && connectDB.readyState === 1) {
+    res.status = 200;
+    res.json({ message: "MongoDB is connected" });
+  } else {
+    res.status = 500;
+    res.json({ message: "MongoDB is not connected" });
+  }
+});
 
 app.use("/api/users", userRoutes);
 
