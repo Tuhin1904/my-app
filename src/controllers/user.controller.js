ce4f00601810d4e66654cb22af4184295f3627e1
Tuhin1904 <iamtuhinn@gmail.com>
2025-11-25 12:54:38 +0530

    test

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -47,7 +47,7 @@ async function register(req, res) {
       message: "Registration failed",
       error: err.message,
       stack: err.stack,
-      mongoStatus: statusMap[mongoose.connection.readyState],
+      mongoUri: process.env.MONGO_URI,
     });
     // return res.status(500).json({
     //   message: "Registration failed",
